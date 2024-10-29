309ac74d8ca6c31fb63830aa9a1589af57eb2b33
Marta Pérez Villamandos <martapervil@mar-101-78.eduroamcm.upf.edu>
2024-10-29 12:35:58 +0100

    script modified to accept as number of files as the user want

--- countlines.sh
+++ countlines.sh
@@ -1,9 +1,12 @@
-if [[ $(cat $1 | wc -l) -eq 0 ]]
-then 
-	echo El archivo $1 stá vacío 
-elif [[ $(cat $1 | wc -l) -eq 1 ]]
-then 
-	echo El archivo $1 iene 1 línea
-else 
-	echo El archivo $1 iene $(cat $1 | wc -l) líneas
-fi
+for i in $@
+do
+	if [[ $(cat $i | wc -l) -eq 0 ]]
+	then 
+		echo El archivo $i stá vacío 
+	elif [[ $(cat $i | wc -l) -eq 1 ]]
+	then 
+		echo El archivo $i iene 1 línea
+	else 
+		echo El archivo $i iene $(cat $i | wc -l) líneas
+	fi
+done
